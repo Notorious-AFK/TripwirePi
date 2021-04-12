b461d935ebe2c7e690f938b76c1af22f6ee17143
Notorious-AFK <57632518+Notorious-AFK@users.noreply.github.com>
2021-04-12 17:47:03 +0200

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+ 
+# Adjust additional repositories
+# apt-add-repository ppa:tista/adapta # Adapta theme repo
+
+ 
+# Get the latest package lists
+apt-get update && apt upgrade
+ 
+# Get DEB files NO NEED
+# wget https://prerelease.keybase.io/keybase_amd64.deb
+ 
+# Install from Repo
+apt-get install psad -y
+apt-get install openssh-server -y
+apt-get install unattended-upgrades -y
+
+ 
+# Install DEB files
+# dpkg -i keybase_amd64.deb
+#apt --fix-broken install -y # Remediate possible issues
+ 
+# Clean up DEB files
+# rm -f keybase_amd64.deb
+ 
+
+# Final message
+echo All applications have been installed, the script will now quit.
+ 
+# Exit the script
+exit 0
